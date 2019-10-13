fd2c01f588bd0bf68c8047f263d3126fcaa111b5
Jack Dwyer <jdwy215@gmail.com>
2019-10-12 23:06:24 -0400

    adding casewhen

--- Lesson3/src/goldfish.kt
+++ Lesson3/src/goldfish.kt
@@ -1,8 +1,10 @@
 import java.util.*
 
 fun main(args: Array<String>) {
-    println("Hello World")
+    println("Hello World ${args[0]}")
     dayOfWeek()
+    conditionals()
+    caseWhen(args[1])
 }
 
 fun dayOfWeek() {
@@ -14,5 +16,16 @@ fun dayOfWeek() {
     
     }")
     println(Calendar.getInstance().get(Calendar.DAY_OF_WEEK))
+}
 
+fun conditionals() {
+    var blem = if ("jack".length > 4) true else false
+    println("conditional: $blem")
+}
+
+fun caseWhen(hour: String) {
+    when(hour.toInt()) {
+        23 -> print("23:00")
+        else -> print("(not) 23:00")
+    }
 }
