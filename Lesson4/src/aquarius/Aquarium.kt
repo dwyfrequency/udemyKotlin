f708c93e143ed565e759082148a31e7bd6a8579e
Jack Dwyer <jdwy215@gmail.com>
2019-10-16 18:26:40 -0400

    adding initial class

---
+++ Lesson4/src/aquarius/Aquarium.kt
@@ -0,0 +1,11 @@
+package aquarius
+
+class Aquarium {
+    val width: Int = 20
+    val height: Int = 40
+    val length: Int = 100
+
+    fun volume(): Int {
+        return length + 30
+    }
+}
---
+++ Lesson4/src/aquarius/start.kt
@@ -0,0 +1,11 @@
+package aquarius
+
+fun main() {
+    buildAquarium()
+}
+
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    println(myAquarium.length)
+    println(myAquarium.volume())
+}
