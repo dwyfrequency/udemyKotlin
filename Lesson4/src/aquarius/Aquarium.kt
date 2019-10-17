4241f23c6ec9796cc4383c814ae78a248bf303c4
Jack Dwyer <jdwy215@gmail.com>
2019-10-17 17:03:31 -0400

    adding subclass example

--- Lesson4/src/aquarius/Aquarium.kt
+++ Lesson4/src/aquarius/Aquarium.kt
@@ -1,15 +1,18 @@
 package aquarius
 
+import kotlin.math.PI
+
 //default constructor
-class Aquarium(var width: Int = 20,
+// must add open to make it subclassable
+open class Aquarium(var width: Int = 20,
                var height: Int = 40,
                var length: Int = 100) {
 
-    var volume: Int
+    open var volume: Int
         get() {return width * height * length / 1000}
         set(value) {height = (value * 1000)/ (width * height)}
 
-    var water = volume * 0.9
+    open var water = volume * 0.9
 
     // alternative constructor
     constructor(numberOfFish: Int): this() {
@@ -17,4 +20,13 @@ class Aquarium(var width: Int = 20,
         val tank: Double = water *2 *0.1
         height = (tank / (length*width)).toInt()
     }
+}
+
+class TowerTank(): Aquarium() {
+    override var water = volume * 0.8
+
+    override var volume: Int
+        get() {return (width * height * length / 1000 * PI).toInt()}
+        set(value) {height = (value * 1000)/ (width * height)}
+
 }
